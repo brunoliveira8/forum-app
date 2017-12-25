ff0f654978893d661480f40135efac8a9b4a549f
Bruno Oliveira <tarcisiobrunooliveira@gmail.com>
2017-12-25 15:04:55 -0300

    Add Question Model

--- backend/forum/models.py
+++ backend/forum/models.py
@@ -8,3 +8,10 @@ class Topic(models.Model):
 
     def __str__(self):
         return self.slug
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=100, blank=False)
+    description = models.TextField(blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
