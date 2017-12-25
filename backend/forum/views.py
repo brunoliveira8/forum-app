542936ff339e0576255b7a8e8b8c1bae1a75933b
Bruno Oliveira <tarcisiobrunooliveira@gmail.com>
2017-12-25 17:39:46 -0300

    Add question serializer and view

--- backend/forum/views.py
+++ backend/forum/views.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets
 
-from .models import Topic
-from .serializers import TopicSerializer
+from .models import Topic, Question
+from .serializers import TopicSerializer, QuestionSerializer
 
 
 # Create your views here.
@@ -12,3 +12,14 @@ class TopicViewSet(viewsets.ReadOnlyModelViewSet):
     """
     queryset = Topic.objects.all()
     serializer_class = TopicSerializer
+
+
+class QuestionViewSet(viewsets.ModelViewSet):
+    """
+    A simple ViewSet for viewing questions.
+    """
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
