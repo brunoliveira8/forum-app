efa4ac9bec556459eceb83b706d72e1058060f4a
Bruno Oliveira <tarcisiobrunooliveira@gmail.com>
2017-12-29 13:57:07 -0300

    Add settings to deploy API

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'dbu+i#v-4uzz=$3#crx)kkqiukx+zrvf1pe3b*q%@lc$sjy@q_'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'true') in ['true', 'True', 'yes']
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [ os.getenv('ALLOWED_HOST', '*'), ]
 
 
 # Application definition
@@ -133,6 +133,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/var/www/static'
+STATICFILES_DIRS = [STATIC_DIR, ]
 
 # DRF Settings
 REST_FRAMEWORK = {
